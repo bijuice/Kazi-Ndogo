d943a91a7426cf8db96fe076210fa1a35ab8ceaf
wh15k3rs <sandraomoi123@gmail.com>
2020-07-01 17:48:40 +0300

    Edited Splash screen

--- app/src/main/java/com/example/myapplication1/Splash_Screen.java
+++ app/src/main/java/com/example/myapplication1/Splash_Screen.java
@@ -13,6 +13,5 @@ public class Splash_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash__screen);
-        //comment to dharmik
     }
 }
