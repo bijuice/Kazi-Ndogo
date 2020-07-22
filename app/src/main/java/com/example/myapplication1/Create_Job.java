5cb2fec75c449ff2adba7a7a81f25972b4a7f509
bijuice <abdul.r.zaid@gmail.com>
2020-07-22 21:09:02 +0300

    Create an array for spinner

--- app/src/main/java/com/example/myapplication1/Create_Job.java
+++ app/src/main/java/com/example/myapplication1/Create_Job.java
@@ -1,14 +1,36 @@
 package com.example.myapplication1;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class  Create_Job  extends AppCompatActivity {
+    EditText jobtitle,paid,jobdesc;
+    Spinner spinner;
+    Button createjob;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_job);
+
+        jobtitle = findViewById(R.id.jobtitle);
+        paid = findViewById(R.id.paid);
+        jobdesc = findViewById(R.id.jobdesc);
+        spinner = findViewById(R.id.spinner);
+        createjob = findViewById(R.id.createjob);
+
+
+
+
+
     }
 }
